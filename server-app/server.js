db0861f9f48a1590319d0184aabd26be0d2aab6c
ino ermes <hikigayahachiman.akame.kill@gmail.com>
2023-12-08 10:46:01 +0700

    big update

--- server-app/server.js
+++ server-app/server.js
@@ -1,47 +1,76 @@
-const app = require('express')()
-const htpp = require('http').createServer(app);
-const io = require('socket.io')(htpp)
-const url = require('url');
-const bodyParser = require('body-parse');
-const { response } = require('express');
+const { randomUUID } = require('crypto');
+const express = require('express');
+const app = express();
+app.use(express.json());
 
-app.use(bodyParser());
 
-let clientResponseRef = null;
-app.get('/*', (req, res) => {
-    const pathname = url.parse(req.url).pathname;
+const htpp = require('http');
+const server = htpp.createServer(app);
+const socket = require('socket.io');
+io = socket(server);
 
-    const obj = {
-        pathname: pathname,
-        method: 'get',
-        params: req.query,
-    };
+const multer = require("multer");
+const upload = multer({
+    dest: "C:/Users/Mio/Downloads/Programs",
+  });
 
-    io.emit('page-request');
-    clientResponseRef = res;
-});
+require('dotenv').config();
+const cloudinary = require('cloudinary').v2;
+cloudinary.config({
+    cloud_name: process.env.CLOUD_NAME,
+    api_key: process.env.API_KEY,
+    api_secret: process.env.API_SECRET,
+    secure: true,
+})
 
-app.post('/*', (req, res) => {
-    const pathname = url.parse(req.url).pathname;
+const clientResponseRefs = new Map();
 
-    const obj = {
-        pathname: pathname,
-        method: 'post',
-        params: req.body,
-    };
+app.use('*', upload.single('file') , async (req, res) => {
+    const _id = randomUUID();
 
-    io.emit('page-request', obj);
-    clientResponseRef = res;
-});
+    forwadrReq = {
+        path: req.baseUrl,
+        method: req.method,
+        headers: req.headers,
+        body: req.body,
+        args: req.query,
+        cookies: req.cookies,
+        _id
+    }
+
+    file = req.file;
+    console.log(file);
+    if(file) {
+        const {secure_url} = await cloudinary.uploader.upload(file.path);
+        forwadrReq.body['image_url'] = secure_url;
+        forwadrReq.headers['Content-Type'] = 'application/json';
+    }
+    delete forwadrReq.headers['content-length'];
+
+    io.emit('forward-request', forwadrReq);
+    clientResponseRefs.set(_id, res);
+    setTimeout(() => {
+        if(clientResponseRefs.has(_id)) {
+            clientResponseRefs.get(_id).status(500).json({
+                'message': 'something went wrong',
+                'status code': '500',
+            });
+            clientResponseRefs.delete(_id);
+        }
+    }, 20000);
+})
 
 io.on('connection', (socket) => {
     console.log('a node connected');
-    socket.on('page-response', (res) => {
-        clientResponseRef.send(res)
+    socket.on('forward-response', (res) => {
+        if(clientResponseRefs.has(res._id)) {
+            clientResponseRefs.get(res._id).status(res.statusCode).json(res.data);
+            clientResponseRefs.delete(res._id);
+        }
     })
 });
 
 var server_port = process.env.PORT || 3000;
-htpp.listen(server_port, () => {
+server.listen(server_port, () => {
     console.log('listening on: ' + server_port);
 });
